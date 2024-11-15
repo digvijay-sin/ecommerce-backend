b602663704eb6042b977ca31867c7039b1551107
digvijay <digvijaysingh47764@gmail.com>
2024-11-15 18:50:15 +0530

    Project Setup

---
+++ ecommerce-app-backend/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce_app_backend.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult APIOne() {
+            return Ok();
+        }
+    }
+}
